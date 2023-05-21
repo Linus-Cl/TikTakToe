95a9b22c9cf5cab1a8373b4f38609085f6311ba9
Linus Claußen <l.claussen@tu-braunschweig.de>
2023-05-21 13:15:02 +0200

    weird return error getChoords()

--- TikTakToe/Program.cs
+++ TikTakToe/Program.cs
@@ -11,8 +11,9 @@ namespace TikTakToe
         static void Main(string[] args)
         {
             const int playerNumber = 2;
-            Map m = new Map(3);
-            m.printMap();
+            const int mapSize = 3;
+            Map m = new Map(mapSize);
+            // m.printMap();
             Player[] players = new Player[playerNumber];
             players = createPlayers(playerNumber);
 
@@ -20,9 +21,38 @@ namespace TikTakToe
             {
                 int activePlayerIndex = i % playerNumber;
                 Player activeplayer = players[activePlayerIndex];
-                Console.WriteLine("Player " + (activePlayerIndex+1) + "'s turn.");
+                Console.WriteLine("Player " + (activePlayerIndex + 1) + "'s turn.");
+                bool validCoords = false;
 
+                int[] coords = { 0, 0 };
+
+
+                while (!validCoords)
+                {
+                    coords = getCoords();
+
+                    if (coords[0] < mapSize && coords[1] < mapSize)
+                    {
+                        validCoords = true;
+                        break;
+                    }
+                    Console.WriteLine("Invalid coordinates! Try again.");
+                }
+
+                m.field[coords[0], coords[1]]= activePlayerIndex;
             }
+        }
+
+         static int[] getCoords()
+        {
+            int[] coords = new int[2];
+            Console.WriteLine("Choose wich field to mark:");
+            Console.WriteLine("x-coordinate (starting with 0): ");
+            coords[0] = (int) Console.Read();
+            Console.WriteLine("y-coordinate (starting with 0): ");
+            coords[1] = (int) Console.Read();
+
+            return coords;
         }
 
         static Player[] createPlayers(int num)
